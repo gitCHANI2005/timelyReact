adf2cbe94c534ec037393df2c07b882d71df6e98
chaniFainendler <chani20058664@gmail.com>
2025-02-26 01:21:40 +0200

    צ

--- ProductController.cs
+++ ProductController.cs
@@ -5,7 +5,7 @@ using Service.Interfaces;
 using System.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+//'
 namespace LayerProject.Controllers
 {
     [Route("api/[controller]")]
