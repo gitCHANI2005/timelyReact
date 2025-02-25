15d7c99d98b9e3900c39e8d7e64893b16552f823
gitCHANI2005 <chani20058664@gmail.com>
2025-02-26 00:10:22 +0200

    page c# yet not ready

---
+++ ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository.Entities;
+using Service.Dtos;
+using Service.Interfaces;
+using System.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LayerProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IService<ProductDto> _productService;
+        public static string _directory = Environment.CurrentDirectory + "/images/";
+        public ProductController(IService<ProductDto> productService)
+        {
+            _productService = productService;
+        }
+    
+        // GET: api/<ProductController>
+        [HttpGet]
+        public List<ProductDto> Get()
+        {
+            return _productService.getAll();
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public ProductDto Get(int id)
+        {
+            return _productService.getById(id);
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public ProductDto Post([FromForm] ProductDto value)
+        {
+            var filePath = Path.Combine(Environment.CurrentDirectory , "Images/", value.File.FileName); //l:/...
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                value.File.CopyTo(fs);
+               // fs.Close();
+            }
+            //
+         return   _productService.addItem(value);
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
